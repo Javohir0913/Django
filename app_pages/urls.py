e4267e8cf9c803c5e32dad0fb9950a52f1c43207
Javohir0913 <pro100java01@gmail.com>
2024-03-06 20:27:01 +0500

    uy ishi

--- app_pages/urls.py
+++ app_pages/urls.py
@@ -1,12 +1,11 @@
-# myapp/urls.py
 from django.urls import path
-from .views import sahifa_1, sahifa_5, sahifa_4, sahifa_3, sahifa_2, HomePageView
+from .views import HomePageView, create_page
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('page/1', sahifa_1, name='sahifa_1'),
-    path('page/2', sahifa_2, name='sahifa_2'),
-    path('page/3', sahifa_3, name='sahifa_3'),
-    path('page/4', sahifa_4, name='sahifa_4'),
-    path('page/5', sahifa_5, name='sahifa_5'),
+    path('page/1', create_page, {'page_number': 1, 'next_page': 2}, name='page_1'),
+    path('page/2', create_page, {'page_number': 2, 'prev_page': 1, 'next_page': 3}, name='page_2'),
+    path('page/3', create_page, {'page_number': 3, 'prev_page': 2, 'next_page': 4}, name='page_3'),
+    path('page/4', create_page, {'page_number': 4, 'prev_page': 3, 'next_page': 5}, name="page_4"),
+    path('page/5', create_page, {'page_number': 5, 'prev_page': 4}, name='page_5'),
 ]
--- app_pages/views.py
+++ app_pages/views.py
@@ -7,29 +7,5 @@ class HomePageView(TemplateView):
     template_name = 'home.html'
 
 
-def sahifa_1(request):
-    return HttpResponse('<h1>1 - sahifa</h1><br>'
-                        '<a href=2>keyingi sahifa</a>')
-
-
-def sahifa_2(request):
-    return HttpResponse('<h1>2 - sahifa</h1><br>'
-                        '<a href=1>oldingi sahifa</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-                        '<a href=3>keyingi sahifa</a>')
-
-
-def sahifa_3(request):
-    return HttpResponse('<h1>3 - sahifa</h1><br>'
-                        '<a href=2>oldingi sahifa</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-                        '<a href=4>keyingi sahifa</a>')
-
-
-def sahifa_4(request):
-    return HttpResponse('<h1>4 - sahifa</h1><br>'
-                        '<a href=3>oldingi sahifa</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-                        '<a href=5>keyingi sahifa</a>')
-
-
-def sahifa_5(request):
-    return HttpResponse('<h1>5 - sahifa</h1><br>'
-                        '<a href=4>oldingi sahifa</a>')
+def create_page(request, page_number, prev_page=None, next_page=None):
+    return render(request, 'page.html', {'page_number': page_number, 'prev_page': prev_page, 'next_page': next_page})
